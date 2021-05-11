92c6a7a744407afe2af8288915a1b92e3e8a6caf
Waleed Butt <56509403+waleedbutt98@users.noreply.github.com>
2021-05-11 13:20:19 +0500

    Styling...

--- source-code/adminportal/src/components/login.js
+++ source-code/adminportal/src/components/login.js
@@ -13,17 +13,27 @@ const Login = () => {
               <p>Login</p>
           </div>
           <div className="login_form">
-              <div className="login_input">
-                  <label for="email">Email</label>
-                  <input type="email" id="email" className="form-control" placeholder="Enter Email"/>
+            <div className="login_radio">
+              <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="type" id="login_radio" value="Admin"/>
+                <label className="form-check-label" for="inlineRadio1">Admin</label>
               </div>
-              <div className="login_input">
-                  <label for="pwd">Password</label>
-                  <input type="email" id="pwd" className="form-control" placeholder="Enter Password"/>
+              <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="type" id="login_radio" value="Employee"/>
+                <label className="form-check-label" for="inlineRadio2">Employee</label>
               </div>
-              <div className="login_submit">
-                  <button type="submit" onclick="login()">Login</button>
-              </div>
+            </div>
+            <div className="login_input">
+                <label for="email">Email</label>
+                <input type="email" id="email" className="form-control" placeholder="Enter Email"/>
+            </div>
+            <div className="login_input">
+                <label for="pwd">Password</label>
+                <input type="email" id="pwd" className="form-control" placeholder="Enter Password"/>
+            </div>
+            <div className="login_submit">
+                <button type="submit" onclick="login()">Login</button>
+            </div>
           </div>
       </div>
     </div>
